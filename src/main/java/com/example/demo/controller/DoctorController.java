a8aced1950b5c323343758fd1ae0eb500c8c9143
Sergio Ayala <sergio.ayala@aldeamo.com>
2022-10-03 23:21:01 -0500

    se agregan cambios camel para la api

--- src/main/java/com/example/demo/controller/DoctorController.java
+++ src/main/java/com/example/demo/controller/DoctorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/doctor")
+@RequestMapping("/api/Doctor")
 public class DoctorController {
 
     @Autowired
